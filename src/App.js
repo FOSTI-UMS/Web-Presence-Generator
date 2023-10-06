c53e07175a0b66b74b407008f0d30084201668ee
Zavier Ferodova Al Fitroh <zavierferodova@gmail.com>
2023-10-06 16:00:14 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+// Import necessary libraries
+import React, { useState, useRef } from 'react';
+import { QRCode } from 'react-qrcode-logo';
+import base64 from 'base-64';
+import html2canvas from 'html2canvas';
+import logo from './images/logo-fosti.webp'
+import logoQR from './images/logo-fosti-qr.webp'; // Update the path based on your folder structure
 
-function App() {
+// Functional component for the QR Code generator
+const QRCodeGenerator = () => {
+  // State variables for form fields and QR code data
+  const [studentId, setStudentId] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [qrCodeData, setQrCodeData] = useState('');
+  const [error, setError] = useState('');
+  const qrCodeRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Input validation
+    if (studentId.includes('/') || studentId.includes(';') ||
+        fullName.includes('/') || fullName.includes(';') ||
+        email.includes('/') || email.includes(';')) {
+      setError("Input cannot contain '/' or ';'");
+      setQrCodeData('');
+      return;
+    }
+
+    const encodedData = base64.encode(`${studentId}/${fullName}/${email}/FOSTI`);
+    setQrCodeData(encodedData);
+    setError('');
+  };
+
+  const handleDownload = () => {
+    if (qrCodeRef.current) {
+      html2canvas(qrCodeRef.current).then((canvas) => {
+        const link = document.createElement('a');
+        link.download = 'qrcode.png';
+        link.href = canvas.toDataURL('image/png');
+        link.click();
+      });
+    } else {
+      console.error('QR code reference is null. Make sure it is properly assigned.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex flex-col items-center justify-center min-h-screen py-9">
+      <div>
+        <img src={logo} alt="Logo FOSTI UMS" className="w-36 h-36 mx-auto mb-4" />
+      </div>
+      <h1 className="text-2xl font-bold mb-4 text-center">QR Presence Generator</h1>
+
+      <form onSubmit={handleSubmit} className="mb-8 w-full max-w-md">
+        <div className="mb-4">
+          <label htmlFor="studentId" className="block text-sm font-medium text-gray-600">
+            Student ID
+          </label>
+          <input
+            type="text"
+            id="studentId"
+            value={studentId}
+            onChange={(e) => setStudentId(e.target.value)}
+            className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="fullName" className="block text-sm font-medium text-gray-600">
+            Full Name
+          </label>
+          <input
+            type="text"
+            id="fullName"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-600">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 w-full"
         >
-          Learn React
-        </a>
-      </header>
+          Generate QR Code
+        </button>
+
+        {error && <p className="text-red-500 mt-2">{error}</p>}
+      </form>
+
+      {qrCodeData && (
+        <div ref={qrCodeRef} className="text-center max-w-xl mx-auto relative">
+          <h2 className="text-xl font-bold mb-2">FOSTI Presence Code</h2>
+          <QRCode
+            value={qrCodeData}
+            logoImage={logoQR}
+            removeQrCodeBehindLogo
+            logoPaddingStyl="circle"
+            size={256}
+          />
+        </div>
+      )}
+
+      {qrCodeData && (
+        <button
+          className="bg-green-500 text-white p-2 rounded-md hover:bg-green-600 mt-4"
+          onClick={handleDownload}
+        >
+          Download QR Code
+        </button>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default QRCodeGenerator;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, screen, fireEvent, waitFor, act } from '@testing-library/react';
+import QRCodeGenerator from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('QRCodeGenerator', () => {
+  test('renders QRCodeGenerator component', () => {
+    render(<QRCodeGenerator />);
+    
+    // Check if the component renders without crashing
+    const headingElement = screen.getByText(/QR Presence Generator/i);
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test('disallows slash in input fields', () => {
+    render(<QRCodeGenerator />);
+
+    fireEvent.change(screen.getByLabelText(/Student ID/i), { target: { value: '123/45' } });
+    fireEvent.change(screen.getByLabelText(/Full Name/i), { target: { value: 'John/Doe' } });
+    fireEvent.change(screen.getByLabelText(/Email/i), { target: { value: 'john@example.com' } });
+
+    const submitButton = screen.getByText(/Generate QR Code/i);
+    fireEvent.click(submitButton);
+
+    const errorElement = screen.getByText(/Input cannot contain/i);
+    expect(errorElement).toBeInTheDocument();
+  });
+
+  test('disallows semicolon in input fields', () => {
+    render(<QRCodeGenerator />);
+
+    fireEvent.change(screen.getByLabelText(/Student ID/i), { target: { value: '12345' } });
+    fireEvent.change(screen.getByLabelText(/Full Name/i), { target: { value: 'John;Doe' } });
+    fireEvent.change(screen.getByLabelText(/Email/i), { target: { value: 'john@example.com' } });
+
+    const submitButton = screen.getByText(/Generate QR Code/i);
+    fireEvent.click(submitButton);
+
+    const errorElement = screen.getByText(/Input cannot contain/i);
+    expect(errorElement).toBeInTheDocument();
+  });
 });
